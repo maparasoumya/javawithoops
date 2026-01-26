67f147877f854ba86509aa9e78cfb73275e47c0f
maparasoumya <soumyamapara0@gmail.com>
2026-01-26 16:04:17 +0530

    Create Primitive.java

---
+++ Primitive.java
@@ -0,0 +1,38 @@
+class Primitive {
+    public static void main(String[] args) {
+
+        // 1. byte (1 byte)
+        byte age = 20;
+
+        // 2. short (2 bytes)
+        short year = 2026;
+
+        // 3. int (4 bytes)
+        int totalstudents = 5000;
+
+        // 4. long (8 bytes)
+        long population = 1400000000L;
+
+        // 5. float (4 bytes)
+        float percentage = 87.5f;
+
+        // 6. double (8 bytes)
+        double cgpa = 8.756;
+
+        // 7. char (2 bytes)
+        char grade = 'A';
+
+        // 8. boolean (1 bit logically)
+        boolean ispassed = true;
+
+        // Printing all values
+        System.out.println("byte: " + age);
+        System.out.println("short: " + year);
+        System.out.println("int: " + totalstudents);
+        System.out.println("long: " + population);
+        System.out.println("float: " + percentage);
+        System.out.println("double: " + cgpa);
+        System.out.println("char: " + grade);
+        System.out.println("boolean: " + ispassed);
+    }
+}
