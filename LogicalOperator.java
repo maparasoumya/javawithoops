74852020dbbf7f35540554dcc008952ef919e0c5
maparasoumya <soumyamapara0@gmail.com>
2026-01-27 12:26:47 +0530

    Create LogicalOperator class with logical operations
    Implement LogicalOperator class with main method to demonstrate logical operations.

---
+++ LogicalOperator.java
@@ -0,0 +1,10 @@
+class LogicalOperator {
+    public static void main(String[] args) {
+        int a =10;
+        System.out.println(a<10 && a>0);
+        System.out.println(a>5 && a<11);
+        System.out.println(a<10 || a>0);
+        System.out.println(!(a<10 || a>0));
+        
+        }
+}
