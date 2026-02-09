af82d2a397102fd84c54816855b628baf5005d4b
maparasoumya <soumyamapara0@gmail.com>
2026-02-09 11:23:00 +0530

    Create DMD.java

---
+++ DMD.java
@@ -0,0 +1,25 @@
+#dynamic method dispatch
+class Animal{
+	void show(){
+		System.out.println("Animal class");
+	}
+}
+class Dog extends Animal{
+	void show(){
+		System.out.println("Dog class");
+	}
+}
+class Cat extends Animal{
+	void show(){
+		System.out.println("Cat class");
+	}
+}
+public class Main {
+	public static void main (String Args[]){
+		Animal a;
+		a = new Dog();
+		a.show();
+		a = new Cat();
+		a.show();
+	}
+}
