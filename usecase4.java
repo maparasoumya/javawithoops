98190c3a62a285d9a695fcbf2cb6d8c780bad930
maparasoumya <soumyamapara0@gmail.com>
2026-03-31 13:46:38 +0530

    Create usecase4.java

---
+++ usecase4.java
@@ -0,0 +1,68 @@
+abstract class LibraryItem {
+    private String itemId;
+    private String title;
+    protected boolean isReserved;
+
+    public LibraryItem(String itemId, String title) {
+        this.itemId = itemId;
+        this.title = title;
+        this.isReserved = false;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void showDetails() {
+        System.out.println("ID: " + itemId + ", Title: " + title);
+    }
+
+    abstract void processLoan();
+}
+
+class TextBook extends LibraryItem {
+
+    public TextBook(String itemId, String title) {
+        super(itemId, title);
+    }
+
+    @Override
+    void processLoan() {
+        if (!isReserved) {
+            isReserved = true;
+            System.out.println("Textbook '" + getTitle() + "' issued for 14 days.");
+        } else {
+            System.out.println("Error: '" + getTitle() + "' is already on loan.");
+        }
+    }
+}
+
+class ResearchPaper extends LibraryItem {
+
+    public ResearchPaper(String itemId, String title) {
+        super(itemId, title);
+    }
+
+    @Override
+    void processLoan() {
+        System.out.println("Generating secure PDF download link for '" + getTitle() + "'...");
+    }
+}
+
+public class cs4 {
+    public static void main(String[] args) {
+
+        
+        TextBook book = new TextBook("B101", "Java Core");
+        book.processLoan(); 
+        book.processLoan(); 
+
+        // TC3
+        ResearchPaper paper = new ResearchPaper("R99", "AI Ethics");
+        paper.processLoan();
+    }
+}
